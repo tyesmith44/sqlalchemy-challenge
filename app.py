921adc0a61205f6980d1b7822b7c9e54ff445e64
tyesmith44 <tyesmith44@gmail.com>
2023-07-31 07:50:52 -0700

    change made to app.py

--- app.py
+++ app.py
@@ -1,183 +1,164 @@
 # Import the dependencies.
 from flask import Flask, jsonify
-import numpy as np
-import pandas as pd
-import datetime as dt
 
-# Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-
+import numpy as np
+import pandas as pd
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-# create engine to hawaii.sqlite
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
 # reflect an existing database into a new model
 Base = automap_base()
 
 # reflect the tables
-# reflect the tables
 Base.prepare(autoload_with=engine)
 
-# View all of the classes that automap found
-#Base.classes.keys()
 # Save references to each table
 Measurement = Base.classes.measurement
+
 Station = Base.classes.station
 
-
 # Create our session (link) from Python to the DB
-# Lazy loading
 session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
+
 
 
 #################################################
 # Flask Routes
 #################################################
-# Home Route
+#route("/")
 @app.route("/")
 def home():
-    """Home route that list all my route paths"""
+    """Home with paths"""
     return (
-        f"Welcome to my HI Weather API<br/>"
-        f"Available Routes: <br>"
-        f"api/v1.0/precipitation<br>"
-        f"api/v1.0/stations<br>"
-        f"/api/v1.0/tobs<br>"
-        f"/api/v1.0/start<br>"
-        f"/api/v1.0/start/end<br>"
-        f"note: start and end must be dates in YYYYMMDD format with start and end seperated by '/'"
-    )
+        f"HOME<br/>" 
+        f"these are the possible paths:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        )
+# f"Note: dates must be in YYYMMDD format with start and end seperated by "/"<br/>"
 
-# Precipitation Route
+#route("/api/v1.0/precipitation")
 @app.route("/api/v1.0/precipitation")
-def prcp_output():
-    """Return precipitation for the last 12 months in json format"""
+def precipitation():
+    """Precipitation for last 12 months JSON"""
 
-    # Calculate the date one year from the last date in data set.
-    year_ago = dt.datetime(2017,8,23) - dt.timedelta(days=365)
+    #Calculate the date one year from the last date in data set.
+    year_ago = dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
 
-    # Perform a query to retrieve the data and precipitation scores
-    prcp_query = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
+    #Perform a query to retrieve the data and precipitation scores
+    precp_df = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
     
-    # Close Session
+    #close session
     session.close()
 
-    precip = {date: prcp for date, prcp in prcp_query}
+    #jsonify
+    prcp = {date: prcp for date, prcp in precp_df}
+    return jsonify(prcp)
 
-    """
-    precip = {}
 
-    for date, prcp in prcp_query:
-        precip[date] = prcp
-
-    """
-
-    # return jsonify(precip)
-    return precip
-
-# Station Route
+#route("/api/v1.0/stations")       
 @app.route("/api/v1.0/stations")
-def station_output():
-    """Returns list of stations"""
-    all_stations_results = session.query(Station.station).all()
+def stations():
+    """List of stations"""
+    stations_all_res = session.query(Station.name, Station.station).all()
 
-     # Close Session
+    #close session
     session.close()
 
-    #Convert list of tuples into normal list
-    all_stations = list(np.ravel(all_stations_results))
+    # Convert list of tuples into normal list
+    stations_all = list(np.ravel(stations_all_res))
 
-    #return jsonify all_stations
-    return jsonify(all_stations)
+    #jsonify
+    return jsonify(Stations = stations_all)
 
-#Temperature Route
+
+#route("/api/v1.0/tobs")       
 @app.route("/api/v1.0/tobs")
-def temp_output():
-    """Return the temperatures for the last year for most active station: 'USC00519281'"""
-    
-    # Calculate the date one year from the last date in data set.
-    year_ago = dt.datetime(2017,8,23) - dt.timedelta(days=365)
+def temperature():
+    """Temperatures for the last 12mo at the Waihee Station"""
 
-    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
-    most_active_station_query = session.query(Measurement.tobs).\
-                            filter(Measurement.station == 'USC00519281').\
-                            filter(Measurement.date >= year_ago).all()
-    
-     # Close Session
+    #Calculate the date one year from the last date in data set.
+    year_ago = dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the last 12 months of temperature observation data for this station 
+    most_active_station = session.query(Measurement.tobs).\
+                        filter(Measurement.station == 'USC00519281').\
+                        filter(Measurement.date >= year_ago).all()
+
+    #close session
     session.close()
 
-    # unravel results
-    temperatures = list(np.ravel(most_active_station_query))
+    # Convert list of tuples into normal list
+    temps_active = list(np.ravel(most_active_station))
 
-    # return array of temperatures for the last year for station USC00519281
-    return jsonify(temperatures = temperatures)
+    #jsonify
+    return jsonify(Temperatures = temps_active)
 
-# start and start and end route for temperatures for USC00519281
+
+#route("/api/v1.0/<start>")       
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
-def temp_start_end(start=None, end=None):
-    """Return a JSON list of the minimum temperature, 
-    the average temperature, and the maximum temperature 
-    for a specified start and or a start and end."""    
+def date_start(start=None, end=None):
+    """List of the minimum temp, average temp, and the maximum temp for a given start day."""
 
-    # sel technique
-    sel = [func.min(Measurement.tobs),
-       func.avg(Measurement.tobs),
-       func.max(Measurement.tobs)]
+    sel = [func.min(Measurement.tobs), 
+           func.avg(Measurement.tobs),
+           func.max(Measurement.tobs)]
     
-    # two possible routes
-    # start is provided (without an end)
+    #start only
     if not end: 
     
-        # transform start into date using dt.datetime
-        start = dt.datetime.strptime(start, "%Y%m%d")
-
-        temp_stats_results = session.query(*sel).\
-                filter(Measurement.station == 'USC00519281').\
-                filter(Measurement.date >= start).all()
-
-        # Close Session
-        session.close()
     
-        # unravel using numpy and convert to list (to jsonify)
+        #Query minimum temp, average temp, and the maximum temp for a given start day at most_active
+        temp_stats_results = session.query(*sel).\
+            filter(Measurement.station == 'USC00519281').\
+            filter(Measurement.date >= start).all()
+    
+        #close session
+        session.close()
+
+        #Convert list of tuples into normal list
         temp_stats = list(np.ravel(temp_stats_results))
 
-        # return jsonify version of temp_stats
+        #jsonify
         return jsonify(min_avg_max = temp_stats)
     
-    # start and end are provided
-    # transform start into date using dt.datetime
-    start = dt.datetime.strptime(start, "%Y%m%d")
-    end = dt.datetime.strptime(end, "%Y%m%d")
 
-    temp_stats_results = session.query(*sel).\
-            filter(Measurement.station == 'USC00519281').\
-            filter(Measurement.date >= start).\
-            filter(Measurement.date <= end).all()
-
-    # Close Session
-    session.close()
+    if start and end:
+        start = dt.datetime.strptime(start, '%Y%m%d')
+        end = dt.datetime.strptime(end, '%Y%m%d')
+        #Query minimum temp, average temp, and the maximum temp for a given start day at most_active
+        temp_stats_results = session.query(*sel).\
+                filter(Measurement.station == 'USC00519281').\
+                filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).all()
     
-    # unravel using numpy and convert to list (to jsonify)
-    temp_stats = list(np.ravel(temp_stats_results))
-
-    # return jsonify version of temp_stats
-    return jsonify(min_avg_max = temp_stats)
+        #close session
+        session.close()
+        
+        #Convert list of tuples into normal list
+        temp_stats = list(np.ravel(temp_stats_results))
+       
+        #jsonify
+        return jsonify(min_avg_max = temp_stats)
 
 
 if __name__ == "__main__":
